064b20038db14ffabf5c6a497bdc196cee85d078
Jeph Bayfield <jeph_b@hotmail.com>
2025-01-19 21:10:02 +0000

    upgrade packages and fix null warnings

--- src/EzMoq.EfCore.Tests/Models/Person.cs
+++ src/EzMoq.EfCore.Tests/Models/Person.cs
@@ -4,9 +4,9 @@
     {
         public int Id { get; set; }
 
-        public string FirstName { get; set; }
+        public string FirstName { get; set; } = string.Empty;
 
-        public string LastName { get; set; }
+        public string LastName { get; set; } = string.Empty;
 
         public DateTime DateOfBirth { get; set; }
 
